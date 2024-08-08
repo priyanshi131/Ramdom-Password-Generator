24ca187c2e996de7876ddf0f1bc4f30fbded45ca
Priyanshi Acharya <117939163+priyanshi131@users.noreply.github.com>
2024-08-08 16:13:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const passwordBox=document.getElementById("password");
+const lenght=12;
+
+const upperCase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase="abcdefghijklmnopqrstuvwxyz";
+const number="0123456789";
+const symbol="@#$%^()_+~|[]></-=";
+
+function createPassword(){
+    let password="";
+    password+=upperCase[Math.floor(Math.random()*upperCase.length)];
+    password+=lowerCase[Math.floor(Math.random()*lowerCase.length)];
+    password+=number[Math.floor(Math.random()*number.length)];
+    password+=symbol[Math.floor(Math.random()*symbol.length)];
+
+    while(lenght>password.length){
+        const alChars=upperCase+lowerCase+number+symbol;
+        password+=alChars[Math.floor(Math.random()*alChars.length)];
+    }
+    passwordBox.value=password;
+}
+
+function copyPassword(){
+    passwordBox.select();
+    document.execCommand("copy");
+}
